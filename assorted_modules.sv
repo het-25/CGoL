4ae2be28b6b3a2406c3dbc3e5d8143e99b32ecfb
Evan Kahn <ekahn@hmc.edu>
2016-03-18 17:21:34 -0700

    tentative controller

--- assorted_modules.sv
+++ assorted_modules.sv
@@ -1,4 +1,48 @@
 //==========================================================================
+//                              CONTROLLER
+// RWSelect 1 for write, 0 for read
+// every clock cycle, increment addr
+// every 64 clock cycles is one frame
+// every frame, increment count
+// every 64 frames, switch from read to write for one frame
+//==========================================================================
+
+module controller (input logic ph1, input logic ph2, input logic reset,
+				   output logic RWSelect, output logic [5:0] addr);
+
+	logic [5:0] addr;
+	logic [5:0] count;
+	logic RWSelect;
+
+	always @posedge(reset) begin
+		addr = 6b'000000;
+		count = 6b'000000;
+		RWSelect = 1b'1;
+	end
+
+	always_latch begin
+
+		if (addr == 7b'11111111) begin
+			//increment counter every time the entire matrix is read or written through
+			count <= count + 1b'1;
+			
+			//if we have gone through 64 life cycles, do a write
+			if (count == 6b'000001) RWSelect <= 1b'0;
+			
+			//once the write is over, do a read
+			if (count == 6b'000000) RWSelect <= 1b'1;
+		end
+		
+		if (ph1) begin
+			//increment address value
+			addr = addr + 1b'1;
+		end
+		
+	end
+end
+
+
+//==========================================================================
 //                            DECODER MODULE
 // returns true if the center cell, given by boolean input 'center', should
 // be lit on the next generation (as determined by the 8-bit input 'sides')
@@ -10,7 +54,7 @@ module decoder (input  logic center, input logic [7:0] sides,
 	
 		// sum the bits coming in
 		logic [2:0] sum = sides[0] + sides[1] + sides[2] + sides[3] + sides[4] + sides[5] + sides[6] + sides[7];
-		
+
 		//consider center bits and bits of sum
 		casex ({center, sum})
 			4'bx011:
