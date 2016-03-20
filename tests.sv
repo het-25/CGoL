697c60a2db1d9861da2697f76bb095bae59ad06b
Evan Kahn <ekahn@hmc.edu>
2016-03-20 14:54:28 -0700

    more testvectors for decoder; hopefully LED driver works

--- tests.sv
+++ tests.sv
@@ -4,7 +4,7 @@ module testbench_decoder();
 	logic nexton;
 	
 	logic [31:0] vectornum, errors;
-	logic [9:0] testvectors[100:0];
+	logic [9:0] testvectors[500:0];
 	//10-bit testvector format: {center, sides[7:0]}_nexton
 	decoder dut(center, sides, nexton);
 	
@@ -34,4 +34,36 @@ module testbench_decoder();
 		end
 		$finish;
 	
+endmodule
+
+//not really actually a testbench; will just display stuff on LED matrix
+module testbench_led(input logic clk; output logic[7:0] row; output logic[7:0] col);
+
+	logic [2:0] linenum;
+	logic [7:0] testvectors[7:0];
+
+	//10-bit testvector format: {center, sides[7:0]}_nexton
+	dispcontrol dut(addr, BitIn, ph1, ph2, row, col);
+	
+	always begin
+		ph1 = 1'b1; ph2 = 1'b0; #5; ph1 = 1'b0; ph2 = 1'b1; #5;
+		linenum = 0;
+	end
+	
+	initial begin
+		$readmemb("led.tv", testvectors)
+	end
+	
+	always @(posedge clk) begin
+		BitIn <= testvectors[linenum];
+		ph1 <= 1b'1;
+		ph2 <= 1b'0;
+		linenum <= linenum + 1'b1;
+	end
+	
+	always @(negedge clk) begin
+		ph1 <= 1b'0;
+		ph2 <= 1b'1;
+	end
+		
 endmodule
