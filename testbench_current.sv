22ecf68f3d590a548c08fbd95bb7de40b835abf4
Vaibhav Viswanathan <omahavaibhav@yahoo.com>
2016-03-22 09:45:46 -0700

    created testbench for current state rf

--- testbench_current.sv
+++ testbench_current.sv
@@ -1,31 +1,31 @@
 module testbench_current();
-	logic center;
-	logic [7:0] sides;
-	logic nexton;
+	logic ph1, ph2, regwrite, reset;
+	logic [7:0] wd, rd, rd_exp;
+	logic [3:0] addr;
 	
 	logic [31:0] vectornum, errors;
-	logic [9:0] testvectors[500:0];
+	logic [20:0] testvectors[500:0];
 	//10-bit testvector format: {center, sides[7:0]}_nexton
-	decoder dut(center, sides, nexton);
+	current_state dut(ph1, ph2, regwrite, reset, addr, addr, wd, rd);
 	
 	always begin
 		clk = 1'b1; #5; clk = 1'b0; #5;
 	end
 	
 	initial begin
-		$readmemb("decoder.tv", testvectors)
+		$readmemb("current.tv", testvectors)
 		vectornum = 0; errors = 0;
 	end
 	
-	always @(posedge clk) begin
-		#1; 8{center, sides, nexton_exp} = testvectors[vectornum]
+	always @(posedge ph1) begin
+		#1; {regwrite, reset, addr, wd, rd_exp} = testvectors[vectornum];
 	end
 	
-	always @(negedge clk)
-		if (nexton !== nexton_exp)
+	always @(negedge ph2)
+		if (rd !== rd_exp)
 		begin
-			$display("Error: inputs = %b; %b", center, sides);
-			$display("outputs = %b (%b expected)", nexton, nexton_exp)
+			$display("Error: inputs = %b; %b; %b; %b", regwrite, reset, addr, wd);
+			$display("outputs = %b (%b expected)", rd, rd_exp)
 			errors = errors + 1;
 		end
 		vectornum = vectornum + 1;
