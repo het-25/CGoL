849bf692ced0e2e91a54a51d135cd296d30ee085
Evan Kahn <ekahn@hmc.edu>
2016-03-27 14:18:08 -0700

    Merge branch 'master' into fpga_prototype
    # Conflicts:
    #	assorted_modules.sv
    #	cgol.sv
    #	current_state.sv
    #	prev_state.sv
    #	testbench_overall.sv

--- cgol.sv
+++ cgol.sv
@@ -9,10 +9,32 @@ module cgol #(parameter WIDTH = 8, REGBITS = 3)
    	logic [WIDTH-1:0]   rd1, rd2, rd3, new_r;
 	
 	//instantiate submodules
-	controller		controller1(ph1, ph2, reset, RWSelect, addr);
-	prev_state 		prev_state1(ph1, ph2, RWSelect, reset, addr, addr, wd, rd1, rd2, rd3);
-	decoder_top		top_decoder(rd2, rd1, rd3, new_r);
-	current_state	current_state1(ph1, ph2, ~RWSelect, reset, addr, addr, new_r, wd);
-	dispcontrol		disp_control(addr, wd, ph1, ph2, row, col);
 	
+	all_synth all_synth1(input logic ph1, input logic ph2, input logic reset,
+						 input logic [7:0] rd1, rd2, rd3, wd,
+						 output logic [7:0] row, col,
+						 output logic RWSelect, addr[2:0], output logic [7:0] new_r);
+						 
+	current_state	current_state1(ph1, ph2, ~RWSelect, reset, addr, addr, new_r, wd);	
+	prev_state 		prev_state1(ph1, ph2, RWSelect, reset, addr, addr, wd, rd1, rd2, rd3);
+
+	
+endmodule
+
+//==========================================================================
+//						   ALL SYNTHESIZED MODULES
+//			module containing all logic intended for synthesis
+//
+//
+//==========================================================================
+
+module all_synth (input logic ph1, input logic ph2, input logic reset,
+				  input logic [7:0] rd1, rd2, rd3, wd,
+				  output logic [7:0] row, col,
+				  output logic RWSelect, addr[2:0], output logic [7:0] new_r);
+
+	controller		controller1(ph1, ph2, reset, RWSelect, addr);
+	decoder_top		top_decoder(rd2, rd1, rd3, new_r);
+	dispcontrol		disp_control(addr, wd, ph1, ph2, row, col);
+
 endmodule
