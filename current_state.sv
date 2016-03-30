b94e5c3f8abfd87f8a669cd56dd5fa8a1d790812
Evan Kahn <ekahn@hmc.edu>
2016-03-30 10:57:29 -0700

    added updated current state registers

--- current_state.sv
+++ current_state.sv
@@ -19,17 +19,20 @@ module current_state #(parameter WIDTH = 8, REGBITS = 3)
   // write second port during phase2 (second half-cycle)
   
   always_latch
-	if (ph2 & reset) begin
-    RAM[0] <= 8'b00011000;
-    RAM[1] <= 8'b00110000;
-    RAM[2] <= 8'b00010000;
-    RAM[3] <= 8'b0;
-    RAM[4] <= 8'b0;
-    RAM[5] <= 8'b0;
-    RAM[6] <= 8'b0;
-    RAM[7] <= 8'b0;
+	if (ph2) begin
+		if (reset) begin
+			RAM[0] <= 8'b00011000;
+			RAM[1] <= 8'b00110000;
+			RAM[2] <= 8'b00010000;
+			RAM[3] <= 8'b0;
+			RAM[4] <= 8'b0;
+			RAM[5] <= 8'b0;
+			RAM[6] <= 8'b0;
+			RAM[7] <= 8'b0;
+    	end
+    	else if (regwrite) begin
+    		RAM[wa] <= wd;
+    	end
 	end
-   else if (ph2 & regwrite) RAM[wa] <= wd;
-
   assign rd = RAM[ra];
 endmodule
